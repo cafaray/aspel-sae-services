393fd0569c4999b5acfc4d8bb1c64006e42e0936
Alberto Farias <cafaray@gmail.com>
2022-03-15 10:46:31 +0100

    fix columns for endpont gastos

--- data/schema/pago.js
+++ data/schema/pago.js
@@ -2,13 +2,64 @@
 
 const {getDBInstance} = require('../connection')
 const {PAGOS, LIMIT, CONCEPTOS, PROVEEDORES, FOLIOS} = require('../../configs/config')
-const SQL_FIND_ALL = `SELECT ${LIMIT} p.CVE_PROV, s.NOMBRE, s.RFC, p.REFER, p.NUM_CARGO, c.NUM_CPTO, c.DESCR, c.TIPO, c.CUEN_CONT, 
-    p.AFEC_COI, p.CTLPOL, p.CVE_AUT, p.CVE_BITA, p.CVE_FOLIO, p.CVE_OBS, p.DOCTO, p.ENTREGADA, p.FECHA_APLI, p.FECHA_ENTREGA, p.FECHA_VENC, p.FECHAELAB, p.IMPMON_EXT,
-    p.NO_FACTURA, p.NUM_MONED, p.REF_SIST, p.SIGNO, p.STATUS, p.TCAMBIO, p.TIPO_MOV, p.USUARIO, 
-    f.IMPUESTO1, f.IMPUESTO2, f.IMPUESTO3, f.IMPUESTO4, (f.IMPUESTO1 + f.IMPUESTO2 + f.IMPUESTO3) AS suma_impuestos, (f.IMPUESTO1 + f.IMPUESTO2 + f.IMPUESTO3 - f.IMPUESTO4 + p.IMPORTE) AS importe
+const SQL_FIND_ALL = `SELECT ${LIMIT} trim(p.CVE_PROV) CVE_PROV, s.NOMBRE, s.RFC, p.NUM_CARGO, 
+    c.DESCR, trim(p.REFER) REFER, trim(p.DOCTO) DOCTO, 
+    SUBSTRING(CAST(p.FECHA_APLI AS VARCHAR(24)) FROM 1 FOR 10) FECHA_APLI,
+    SUBSTRING(CAST(p.FECHAELAB AS VARCHAR(24)) FROM 1 FOR 10) FECHAELAB,
+    f.IMPUESTO1, f.IMPUESTO2, f.IMPUESTO3, f.IMPUESTO4, 
+    (f.IMPUESTO1 + f.IMPUESTO2 + f.IMPUESTO3) AS IMPUESTOS, 
+    (f.IMPUESTO1 + f.IMPUESTO2 + f.IMPUESTO3 - f.IMPUESTO4 + p.IMPORTE) AS IMPORTE, 
+    c.NUM_CPTO, c.TIPO, c.CUEN_CONT, p.AFEC_COI, p.CTLPOL, p.CVE_AUT, p.CVE_BITA, trim(p.CVE_FOLIO) CVE_FOLIO, p.CVE_OBS, p.ENTREGADA, 
+    p.FECHA_ENTREGA, p.FECHA_VENC, p.IMPMON_EXT, trim(p.NO_FACTURA) NO_FACTURA, p.NUM_MONED, p.REF_SIST, p.SIGNO, p.STATUS, p.TCAMBIO, p.TIPO_MOV, p.USUARIO
 FROM ${PAGOS} p INNER JOIN ${CONCEPTOS} c ON p.NUM_CPTO = c.NUM_CPTO
     INNER JOIN ${PROVEEDORES} s ON p.CVE_PROV = s.CLAVE
     INNER JOIN ${FOLIOS} f ON p.CVE_FOLIO = f.CVE_FOLIO`
+
+    const SQL_FINDBY_DATE_NOACCOUNTABLE = `SELECT trim(p.CVE_PROV) CVE_PROV, s.NOMBRE, s.RFC, p.NUM_CARGO, 
+    c.DESCR, trim(p.REFER) REFER, trim(p.DOCTO) DOCTO, 
+    SUBSTRING(CAST(p.FECHA_APLI AS VARCHAR(24)) FROM 1 FOR 10) FECHA_APLI,
+    SUBSTRING(CAST(p.FECHAELAB AS VARCHAR(24)) FROM 1 FOR 10) FECHAELAB,
+    f.IMPUESTO1, f.IMPUESTO2, f.IMPUESTO3, f.IMPUESTO4, 
+    (f.IMPUESTO1 + f.IMPUESTO2 + f.IMPUESTO3) AS IMPUESTOS, 
+    (f.IMPUESTO1 + f.IMPUESTO2 + f.IMPUESTO3 - f.IMPUESTO4 + p.IMPORTE) AS IMPORTE, 
+    c.NUM_CPTO, c.TIPO, c.CUEN_CONT, p.AFEC_COI, p.CTLPOL, p.CVE_AUT, p.CVE_BITA, trim(p.CVE_FOLIO) CVE_FOLIO, p.CVE_OBS, p.ENTREGADA, 
+    p.FECHA_ENTREGA, p.FECHA_VENC, p.IMPMON_EXT, trim(p.NO_FACTURA) NO_FACTURA, p.NUM_MONED, p.REF_SIST, p.SIGNO, p.STATUS, p.TCAMBIO, p.TIPO_MOV, p.USUARIO
+FROM ${PAGOS} p INNER JOIN ${CONCEPTOS} c ON p.NUM_CPTO = c.NUM_CPTO
+    INNER JOIN ${PROVEEDORES} s ON p.CVE_PROV = s.CLAVE
+    INNER JOIN ${FOLIOS} f ON p.CVE_FOLIO = f.CVE_FOLIO
+WHERE p.FECHA_APLI >= ? AND p.AFEC_COI <> 'A' AND p.NUM_CPTO NOT IN(1,2,7,9,12,19,20,28) ORDER BY p.FECHA_APLI` 
+
+const SQL_FINDBY_DATE_PROVIDER_NOACCOUNTABLE = `SELECT trim(p.CVE_PROV) CVE_PROV, s.NOMBRE, s.RFC, p.NUM_CARGO, 
+    c.DESCR, trim(p.REFER) REFER, trim(p.DOCTO) DOCTO, 
+    SUBSTRING(CAST(p.FECHA_APLI AS VARCHAR(24)) FROM 1 FOR 10) FECHA_APLI,
+    SUBSTRING(CAST(p.FECHAELAB AS VARCHAR(24)) FROM 1 FOR 10) FECHAELAB,
+    f.IMPUESTO1, f.IMPUESTO2, f.IMPUESTO3, f.IMPUESTO4, 
+    (f.IMPUESTO1 + f.IMPUESTO2 + f.IMPUESTO3) AS IMPUESTOS, 
+    (f.IMPUESTO1 + f.IMPUESTO2 + f.IMPUESTO3 - f.IMPUESTO4 + p.IMPORTE) AS IMPORTE, 
+    c.NUM_CPTO, c.TIPO, c.CUEN_CONT, p.AFEC_COI, p.CTLPOL, p.CVE_AUT, p.CVE_BITA, trim(p.CVE_FOLIO) CVE_FOLIO, p.CVE_OBS, p.ENTREGADA, 
+    p.FECHA_ENTREGA, p.FECHA_VENC, p.IMPMON_EXT, trim(p.NO_FACTURA) NO_FACTURA, p.NUM_MONED, p.REF_SIST, p.SIGNO, p.STATUS, p.TCAMBIO, p.TIPO_MOV, p.USUARIO
+FROM ${PAGOS} p INNER JOIN ${CONCEPTOS} c ON p.NUM_CPTO = c.NUM_CPTO
+    INNER JOIN ${PROVEEDORES} s ON p.CVE_PROV = s.CLAVE
+    LEFT JOIN ${FOLIOS} f ON p.CVE_FOLIO = f.CVE_FOLIO
+WHERE trim(p.CVE_PROV) = ? AND p.FECHA_APLI >= ? AND p.AFEC_COI <> 'A' AND p.NUM_CPTO NOT IN(1,2,7,9,12,19,20,28) ORDER BY p.FECHA_APLI`
+
+const SQL_FINDBY_PROVIDER_NOACCOUNTABLE = `SELECT trim(p.CVE_PROV) CVE_PROV, s.NOMBRE, s.RFC, p.NUM_CARGO, 
+    c.DESCR, trim(p.REFER) REFER, trim(p.DOCTO) DOCTO, 
+    SUBSTRING(CAST(p.FECHA_APLI AS VARCHAR(24)) FROM 1 FOR 10) FECHA_APLI,
+    SUBSTRING(CAST(p.FECHAELAB AS VARCHAR(24)) FROM 1 FOR 10) FECHAELAB,
+    f.IMPUESTO1, f.IMPUESTO2, f.IMPUESTO3, f.IMPUESTO4, 
+    (f.IMPUESTO1 + f.IMPUESTO2 + f.IMPUESTO3) AS IMPUESTOS, 
+    (f.IMPUESTO1 + f.IMPUESTO2 + f.IMPUESTO3 - f.IMPUESTO4 + p.IMPORTE) AS IMPORTE, 
+    c.NUM_CPTO, c.TIPO, c.CUEN_CONT, p.AFEC_COI, p.CTLPOL, p.CVE_AUT, p.CVE_BITA, trim(p.CVE_FOLIO) CVE_FOLIO, p.CVE_OBS, p.ENTREGADA, 
+    p.FECHA_ENTREGA, p.FECHA_VENC, p.IMPMON_EXT, trim(p.NO_FACTURA) NO_FACTURA, p.NUM_MONED, p.REF_SIST, p.SIGNO, p.STATUS, p.TCAMBIO, p.TIPO_MOV, p.USUARIO
+FROM ${PAGOS} p INNER JOIN ${CONCEPTOS} c ON p.NUM_CPTO = c.NUM_CPTO
+    INNER JOIN ${PROVEEDORES} s ON p.CVE_PROV = s.CLAVE
+    LEFT JOIN ${FOLIOS} f ON p.CVE_FOLIO = f.CVE_FOLIO
+WHERE trim(p.CVE_PROV) = ? AND p.NUM_CPTO NOT IN(1,2,7,9,12,19,20,28) ORDER BY p.FECHA_APLI`
+
+const SQL_UPDATE_AFECT_COI = `UPDATE ${PAGOS} SET AFEC_COI = 'A' 
+WHERE trim(CVE_PROV) = ? AND trim(REFER) = ? AND NUM_CARGO = ? AND NUM_CPTO = ?
+RETURNING CVE_PROV, REFER, NUM_CARGO, NUM_CPTO`
 
 const message = `Error getting data.\n===>Error querying at pagos:\n`
 
@@ -30,18 +81,10 @@ async function getAll(cb) {
     }
 }
 
-async function getUnaccountedByDate(appliedDate, cb) {
-    const SQL_FINDBY_DATE_NOACCOUNTABLE = `SELECT ${LIMIT} p.CVE_PROV, s.NOMBRE, s.RFC, p.REFER, p.NUM_CARGO, c.NUM_CPTO, c.DESCR, c.TIPO, c.CUEN_CONT, 
-        p.AFEC_COI, p.CTLPOL, p.CVE_AUT, p.CVE_BITA, p.CVE_FOLIO, p.CVE_OBS, p.DOCTO, p.ENTREGADA, p.FECHA_APLI, p.FECHA_ENTREGA, p.FECHA_VENC, p.FECHAELAB, p.IMPMON_EXT,
-        p.NO_FACTURA, p.NUM_MONED, p.REF_SIST, p.SIGNO, p.STATUS, p.TCAMBIO, p.TIPO_MOV, p.USUARIO, 
-        f.IMPUESTO1, f.IMPUESTO2, f.IMPUESTO3, f.IMPUESTO4, (f.IMPUESTO1 + f.IMPUESTO2 + f.IMPUESTO3) AS suma_impuestos, (f.IMPUESTO1 + f.IMPUESTO2 + f.IMPUESTO3 - f.IMPUESTO4 + p.IMPORTE) AS importe
-    FROM ${PAGOS} p INNER JOIN ${CONCEPTOS} c ON p.NUM_CPTO = c.NUM_CPTO
-        INNER JOIN ${PROVEEDORES} s ON p.CVE_PROV = s.CLAVE
-        INNER JOIN ${FOLIOS} f ON p.CVE_FOLIO = f.CVE_FOLIO
-    WHERE p.FECHA_APLI >= cast('${appliedDate}' as date) AND p.AFEC_COI <> 'A' AND p.NUM_CPTO NOT IN(1,2,7,9,12,19,20,28) ORDER BY p.FECHA_APLI` 
+async function getUnaccountedByDate(appliedDate, cb) {    
     try {
         const dbInstance = await getDBInstance()
-        dbInstance.query(SQL_FINDBY_DATE_NOACCOUNTABLE, (err, data) => {
+        dbInstance.query(SQL_FINDBY_DATE_NOACCOUNTABLE, [appliedDate], (err, data) => {
             if(err){
                 console.log(`${message}${err}`)
                 setImmediate(() => cb(err))
@@ -57,17 +100,10 @@ async function getUnaccountedByDate(appliedDate, cb) {
 }
 
 async function getUnaccountedByDateProvider(appliedDate, idProvider, cb) {
-    const SQL_FINDBY_DATE_PROVIDER_NOACCOUNTABLE = `SELECT ${LIMIT} p.CVE_PROV, s.NOMBRE, s.RFC, p.REFER, p.NUM_CARGO, c.NUM_CPTO, c.DESCR, c.TIPO, c.CUEN_CONT, 
-        p.AFEC_COI, p.CTLPOL, p.CVE_AUT, p.CVE_BITA, p.CVE_FOLIO, p.CVE_OBS, p.DOCTO, p.ENTREGADA, p.FECHA_APLI, p.FECHA_ENTREGA, p.FECHA_VENC, p.FECHAELAB, p.IMPMON_EXT,
-        p.NO_FACTURA, p.NUM_MONED, p.REF_SIST, p.SIGNO, p.STATUS, p.TCAMBIO, p.TIPO_MOV, p.USUARIO, 
-        f.IMPUESTO1, f.IMPUESTO2, f.IMPUESTO3, f.IMPUESTO4, (f.IMPUESTO1 + f.IMPUESTO2 + f.IMPUESTO3) AS suma_impuestos, (f.IMPUESTO1 + f.IMPUESTO2 + f.IMPUESTO3 - f.IMPUESTO4 + p.IMPORTE) AS importe
-    FROM ${PAGOS} p INNER JOIN ${CONCEPTOS} c ON p.NUM_CPTO = c.NUM_CPTO
-        INNER JOIN ${PROVEEDORES} s ON p.CVE_PROV = s.CLAVE
-        LEFT JOIN ${FOLIOS} f ON p.CVE_FOLIO = f.CVE_FOLIO
-    WHERE trim(p.CVE_PROV) = ? AND p.FECHA_APLI >= ? AND p.AFEC_COI <> 'A' AND p.NUM_CPTO NOT IN(1,2,7,9,12,19,20,28) ORDER BY p.FECHA_APLI`
+
     try {
         const dbInstance = await getDBInstance()
-        dbInstance.query(SQL_FINDBY_DATE_PROVIDER_NOACCOUNTABLE, [idProvider, new Date(appliedDate)], (err, data) => {
+        dbInstance.query(SQL_FINDBY_DATE_PROVIDER_NOACCOUNTABLE, [idProvider, appliedDate], (err, data) => {
             if(err){
                 console.log(`${message}${err}`)
                 setImmediate(() => cb(err))
@@ -82,18 +118,10 @@ async function getUnaccountedByDateProvider(appliedDate, idProvider, cb) {
     }
 }
 
-async function getUnaccountedByProvider(idProvider, cb) {
-    const SQL_FINDBY_DATE_PROVIDER_NOACCOUNTABLE = `SELECT ${LIMIT} p.CVE_PROV, s.NOMBRE, s.RFC, p.REFER, p.NUM_CARGO, c.NUM_CPTO, c.DESCR, c.TIPO, c.CUEN_CONT, 
-        p.AFEC_COI, p.CTLPOL, p.CVE_AUT, p.CVE_BITA, p.CVE_FOLIO, p.CVE_OBS, p.DOCTO, p.ENTREGADA, p.FECHA_APLI, p.FECHA_ENTREGA, p.FECHA_VENC, p.FECHAELAB, p.IMPMON_EXT,
-        p.NO_FACTURA, p.NUM_MONED, p.REF_SIST, p.SIGNO, p.STATUS, p.TCAMBIO, p.TIPO_MOV, p.USUARIO, 
-        f.IMPUESTO1, f.IMPUESTO2, f.IMPUESTO3, f.IMPUESTO4, (f.IMPUESTO1 + f.IMPUESTO2 + f.IMPUESTO3) AS suma_impuestos, (f.IMPUESTO1 + f.IMPUESTO2 + f.IMPUESTO3 - f.IMPUESTO4 + p.IMPORTE) AS importe
-    FROM ${PAGOS} p INNER JOIN ${CONCEPTOS} c ON p.NUM_CPTO = c.NUM_CPTO
-        INNER JOIN ${PROVEEDORES} s ON p.CVE_PROV = s.CLAVE
-        LEFT JOIN ${FOLIOS} f ON p.CVE_FOLIO = f.CVE_FOLIO
-    WHERE trim(p.CVE_PROV) = ? AND p.NUM_CPTO NOT IN(1,2,7,9,12,19,20,28) ORDER BY p.FECHA_APLI`
+async function getUnaccountedByProvider(idProvider, cb) {    
     try {
         const dbInstance = await getDBInstance()
-        dbInstance.query(SQL_FINDBY_DATE_PROVIDER_NOACCOUNTABLE, [idProvider], (err, data) => {
+        dbInstance.query(SQL_FINDBY_PROVIDER_NOACCOUNTABLE, [idProvider], (err, data) => {
             if(err){
                 console.log(`${message}${err}`)
                 setImmediate(() => cb(err))
@@ -109,9 +137,6 @@ async function getUnaccountedByProvider(idProvider, cb) {
 }
 
 async function setUpgradeCOI(idProvider, reference, idCharge, idConcept, cb) {
-    const SQL_UPDATE_AFECT_COI = `UPDATE ${PAGOS} SET AFEC_COI = 'A' 
-    WHERE trim(CVE_PROV) = ? AND trim(REFER) = ? AND NUM_CARGO = ? AND NUM_CPTO = ?
-    RETURNING CVE_PROV, REFER, NUM_CARGO, NUM_CPTO`
     try {
         const dbInstance = await getDBInstance()
         dbInstance.query(SQL_UPDATE_AFECT_COI, [idProvider, reference, idCharge, idConcept], (err, data) => {
