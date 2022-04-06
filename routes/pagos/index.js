05e213ca9367f2ffa9c548cd6d6b34cda9038f5b
Alberto Farias <cafaray@gmail.com>
2022-04-06 11:42:15 +0200

    updgrade with happy path changes

--- routes/pagos/index.js
+++ routes/pagos/index.js
@@ -9,15 +9,16 @@ const getPagosByDate = promisify(pagos.getUnaccountedByDate)
 const getPagosByDateProvider = promisify(pagos.getUnaccountedByDateProvider)
 const getPagosByProvider = promisify(pagos.getUnaccountedByProvider)
 const setActualizaCOI = promisify(pagos.setUpgradeCOI)
+const getPagosById = promisify(pagos.getById)
 
 module.exports = async function (fastify, opts) {
     fastify.put('/', async function (request, reply) {
         console.log(`body: ${request.body}`)
-        const {providerId, reference, chargeId, conceptId} = request.body
-        console.log(`providerId= ${providerId}, reference= ${reference}, chargeId= ${chargeId}, conceptId= ${conceptId}`)
+        const {supplierId, reference, chargeId, conceptId} = request.body
+        console.log(`supplierId= ${supplierId}, reference= ${reference}, chargeId= ${chargeId}, conceptId= ${conceptId}`)
         try{
-            if (providerId && reference && chargeId && conceptId){
-                await setActualizaCOI(providerId, reference, chargeId, conceptId)
+            if (supplierId && reference && chargeId && conceptId){
+                await setActualizaCOI(supplierId, reference, chargeId, conceptId)
                 .then((data) => {
                     console.log(`data: ${data}`)
                     reply.status(200).send(JSON.parse(data))
@@ -26,7 +27,7 @@ module.exports = async function (fastify, opts) {
                     reply.status(500).send(err)        
                 })
             } else {
-                reply.status(400).send('Missing fields to identify the payment. Set valid values for: providerId, reference, chargeId and conceptId.')
+                reply.status(400).send('Missing fields to identify the payment. Set valid values for: supplierId, reference, chargeId and conceptId.')
             }
         } catch (err) {
             reply.status(500).send(err)
@@ -35,7 +36,7 @@ module.exports = async function (fastify, opts) {
     fastify.get('/', async function (request, reply) {
         try {
             const url = URL.parse(request.url, true)
-            const {appliedDate} = url.query
+            const {appliedDate} = url.query            
             if (appliedDate){
                 await getPagosByDate(appliedDate)
                 .then((data) => {
@@ -59,14 +60,35 @@ module.exports = async function (fastify, opts) {
             reply.status(500).send(err)
         }
     })
-    fastify.get('/:providerId', async function (request, reply) {
+    fastify.get('/:docto', async function (request, reply) {
         try {
-            const {providerId} = request.params
+            const {docto} = request.params
+            if (docto){
+                await getPagosById(docto)
+                .then((data) => {
+                    // console.log(`data: ${data}`)
+                    reply.status(200).send(JSON.parse(data))
+                })
+                .catch((err) => {
+                    reply.status(500).send(err)
+                })    
+            } else {
+                reply.status(400).send('Bad format. Missing id')
+            }
+        } catch(err) {
+            reply.status(500).send(err)
+        }
+    })
+    fastify.get('/proveedores/:supplierId', async function (request, reply) {
+        try {
+            const {supplierId} = request.params
             const url =  URL.parse(request.url, true)
             const {appliedDate} = url.query
-            if (providerId){
+
+            if (supplierId){
                 if (appliedDate){
-                    await getPagosByDateProvider(appliedDate,providerId)
+                    console.log(`search ${supplierId} and ${appliedDate}`)
+                    await getPagosByDateProvider(appliedDate,supplierId)
                     .then((data) => {
                         // console.log(`data: ${data}`)
                         reply.status(200).send(JSON.parse(data))
@@ -75,7 +97,7 @@ module.exports = async function (fastify, opts) {
                         reply.status(500).send(err)
                     })
                 } else {
-                    await getPagosByProvider(providerId)
+                    await getPagosByProvider(supplierId)
                     .then((data) => {
                         // console.log(`data: ${data}`)
                         reply.status(200).send(JSON.parse(data))
