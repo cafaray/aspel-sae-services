8f7d5122021b402c205837bade8f57d1c9b4e1fb
Alberto Farias <cafaray@gmail.com>
2022-09-07 23:06:11 +0200

    Hot fixes applyed while running production testing

--- config.js
+++ config.js
@@ -1,8 +1,9 @@
 var options = {};
 
-options.host = '127.0.0.1';
+options.host = '192.168.100.40';
 options.port = 3050;
-options.database = '/var/lib/firebird/2.5/data/SAE80EMPRE01.FDB';
+// options.database = '/var/lib/firebird/2.5/data/COI80EMPRE1.FDB';
+options.database = 'C:\\Program Files (x86)\\Common Files\\Aspel\\Sistemas Aspel\\SAE8.00\\Empresa01\\Datos\\SAE80EMPRE01.FDB'
 options.user = 'SYSDBA';
 options.password = 'masterkey';
 options.lowercase_keys = false; // set to true to lowercase keys
